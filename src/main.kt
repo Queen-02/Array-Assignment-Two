276c014e8b734d1d03c1a617e50e908f76e8d98b
Queen-02 <queenzyblack176@gmail.com>
2022-03-18 08:14:49 +0300

    add changes made

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,49 @@
 fun main() {
+    //Question one
+    var result = productOfInts(arrayOf(10, 2, 16))
+    println(result)
+
+    //Question two
+    var sumResult = sumOfDecimals(arrayOf("Lion", 23.5, "Brown", 56, 9.5, 63.2F))
+    println(sumResult)
+
+    //Question three
+    var sounds = characters(arrayOf('a', 'f', 'e', 'o', 'r', 'u', 'p'))
+    println(sounds)
 
 }
 
+//function that takes in an array of integers and returns the product
+fun productOfInts(numbers: Array<Int>): Int{
+    var multiply = 1
+
+    numbers.forEach { number ->
+        multiply*=number
+    }
+    return multiply
+}
+
+//function that takes in mixed types and
+//returns the sum of all the decimal elements
+
+fun sumOfDecimals(mixedArrays: Array<Any>): Number{
+    var sumDec = 0.0
+    mixedArrays.forEach { mixedArray ->
+        if (mixedArray is Double || mixedArray is Float){
+            sumDec+=mixedArray.toString().toDouble()
+        }
+    }
+    return sumDec
+}
+
+//function that takes in an array of characters
+//and returns the number of vowels in the array
+fun characters(vowels: Array<Char>): Int{
+    var char = 0
+    vowels.forEach { vowel ->
+        if (vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u'){
+            ++char
+        }
+    }
+    return char
+}
